941f9d628e6006abdf1c756d521188f04a29fc8c
Setalid <marius.setalid@gmail.com>
2020-11-22 17:59:41 +0100

    Testing github contribution

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask.templating import render_template_string
 import methods
 from flask import Flask, redirect
-import webbrowser
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #season, episode = methods.getRandomSeasonAndEpisode()
-    #print("Loading S%dE%d" % (season, episode))
-    #url = methods.getEpisodeURL(season, episode)
     url = methods.getRandomEpisode()
     print("Episode url is: {}".format(url))
-    # return redirect("https://google.com", code=302)
-    #webbrowser.open_new_tab(url)
     return redirect(url, code=302)
-    #return ('', 204)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
